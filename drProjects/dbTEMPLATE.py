7362d04a9054e4ddab293ab4b13cb3839787497f
Francis Adepoju <francis.adepoju@hotmail.com>
2019-12-07 23:42:34 +0800

    added dbTemplate

---
+++ drProjects/dbTEMPLATE.py
@@ -0,0 +1,12 @@
+"""
+This is a JSON object to hold my local SQL DB connectio.
+Written by:     Francis ADEPOJU
+Date Completed: December 15, 2019
+"""
+mySQL = {
+    'host'          : "127.0.0.1", 
+    'user'          : "root", 
+    'password'      : "root",
+    'database'      : "datarepresentation",
+    'auth_plugin'   : "mysql_native_password"
+}
