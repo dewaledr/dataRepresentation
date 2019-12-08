3d4049351c5b382b88831047546e4a6be18bf203
dewaledr <42543152+dewaledr@users.noreply.github.com>
2019-12-08 21:55:43 +0800

    Update dbTEMPLATE.py
    almost forgot to edit.. problem with copy and paste

--- drProjects/dbTEMPLATE.py
+++ drProjects/dbTEMPLATE.py
@@ -4,9 +4,9 @@ Written by:     Francis ADEPOJU
 Date Completed: December 15, 2019
 """
 mySQL = {
-    'host'          : "127.0.0.1", 
-    'user'          : "root", 
-    'password'      : "root",
-    'database'      : "datarepresentation",
+    'host'          : "hostaddress", 
+    'user'          : "username", 
+    'password'      : "password",
+    'database'      : "databasedname",
     'auth_plugin'   : "mysql_native_password"
 }
